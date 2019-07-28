f11d17db2fe513f89d695ff3794b29eca36569e3
Adrimi <adriano100417@gmail.com>
2019-07-28 16:07:01 +0200

    initial commit

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Adrimi on 28/07/2019.
+//  Copyright © 2019 Adrimi. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
---
+++ Project10/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Adrimi on 22/07/2019.
+//  Copyright © 2019 Adrimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
