6dde10f866309ef42cc6a0f552a97d21c05d55e7
Adrimi <adriano100417@gmail.com>
2019-07-28 16:30:16 +0200

    override func added

--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -16,6 +16,16 @@ class ViewController: UICollectionViewController {
         
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
 
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Uable to dequeue PersonCell")
+        }
+        
+        return cell
+    }
 }
 
