d7f6611384abb8fa2c9649da171984cdc756f1f9
Adrimi <adriano100417@gmail.com>
2019-07-28 22:00:40 +0200

    Added methods to load read and write jpeg data to cached memory. added .gitignore

--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -26,6 +26,47 @@ class ViewController: UICollectionViewController {
         }
         
         return cell
+    }
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        
+        // modifying picture to crop what I want
+        picker.allowsEditing = true
+        
+        // just needed to work fine, thus conforming to two delegates
+        picker.delegate = self
+        
+        present(picker, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // Extract the image from the dictionary that is passed
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        // Generate universally unique identifier (in string)
+        let imageName = UUID().uuidString
+        
+        // Find /documents directory, to save data specific for this app
+        let imagePath = getDocumentDirectory().appendingPathComponent(imageName)
+        
+        // Convert image to jpeg
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            // having valid jpeg data, write it to the disc
+            try? jpegData.write(to: imagePath)
+        }
+        
+        // call out the view controller
+        dismiss(animated: true)
+        
+    }
+    
+    func getDocumentDirectory() -> URL {
+        // default.urls asks for /documents directory, relative to the user's home directory.
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 }
 
